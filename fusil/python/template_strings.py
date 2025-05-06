1db37b83acfe4611bdf4aea773d5530d5c6eb04b
devdanzin <74280297+devdanzin@users.noreply.github.com>
2025-05-06 15:10:43 -0300

    Add template strings as input.

---
+++ fusil/python/template_strings.py
@@ -0,0 +1,21 @@
+import sys
+from ast import literal_eval
+from string.templatelib import Interpolation, Template
+
+from fusil.python.values import SURROGATES, INTERESTING
+from fusil.python.tricky_weird import weird_instance_names, weird_names
+
+sys.set_int_max_str_digits(4305)
+
+TEMPLATES = []
+for value in SURROGATES:
+    TEMPLATES.append(f"""Template({value}, Interpolation({value}, "value"))""")
+
+for value in INTERESTING:
+    TEMPLATES.append(f"""Template("\\x00", Interpolation({value}, "value"))""")
+
+for name in weird_instance_names:
+    TEMPLATES.append(f"""Template("\\x00", Interpolation(weird_instances['{name}'], "name"))""")
+
+for name in weird_names:
+    TEMPLATES.append(f"""Template("\\x00", Interpolation(weird_classes['{name}'], "name"))""")
