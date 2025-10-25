8e2c58406b5a7af8d7241c907d32502f3c60877e
devdanzin <74280297+devdanzin@users.noreply.github.com>
2025-10-25 10:32:27 -0300

    Ignore AtomicInt64 and AtomicDict._rehash again.

--- fusil/python/blacklists.py
+++ fusil/python/blacklists.py
@@ -232,6 +232,7 @@ OBJECT_BLACKLIST = {
     "Lock",
     "RLock",
     "Semaphore",
+    "AtomicInt64"
 }
 METHOD_BLACKLIST = {
     "__class__",
@@ -247,6 +248,7 @@ METHOD_BLACKLIST = {
     "_randbelow",
     "_randbelow_with_getrandbits",
     "_read",
+    "_rehash",
     "_run_once",
     "_serve",
     "_shutdown",
